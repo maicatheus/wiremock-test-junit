3126e6ce23b9bb887970694efa519305693ed05b
matheus_maica <matheus_maica@hotmail.com>
2022-12-14 18:03:01 -0300

    Service e controller

--- src/main/java/com/wiremock/wiremock/controller/UniversityController.java
+++ src/main/java/com/wiremock/wiremock/controller/UniversityController.java
@@ -1,12 +1,13 @@
 package com.wiremock.wiremock.controller;
 
 import com.wiremock.wiremock.dto.UniversityDTO;
-import groovy.util.logging.Slf4j;
+import com.wiremock.wiremock.service.UniversityService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
