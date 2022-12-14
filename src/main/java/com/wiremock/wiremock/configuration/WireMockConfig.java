ca034b90c093737beb40511924e837e875385c82
matheus_maica <matheus_maica@hotmail.com>
2022-12-14 17:28:05 -0300

    WireMockConfig - Será criado futuramente o WireMockProxy

---
+++ src/main/java/com/wiremock/wiremock/configuration/WireMockConfig.java
@@ -0,0 +1,16 @@
+package com.wiremock.wiremock.configuration;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@ConfigurationProperties(prefix = "wiremock-config")
+@Data
+@Profile("integration")
+public class WireMockConfig {
+
+//    private List<WireMockProxy> proxies;
+
+}
